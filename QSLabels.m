6b7b539e5414d9be837bbfb79bd212eca3279092
Mike Ho <mike@quasidea.com>
2011-01-07 14:54:53 -0800

    Initial Set of Cleanups

--- QSLabels.m
+++ QSLabels.m
@@ -1,17 +1,31 @@
-//
-//  Labels.m
-//  iVQ
-//
-//  Created by Mike Ho on 9/28/10.
-//  Copyright 2010 Quasidea Development, LLC. All rights reserved.
-//
+/**
+ * QSLabels.m
+ * 
+ * Copyright (c) 2010 - 2011, Quasidea Development, LLC
+ * For more information, please go to http://www.quasidea.com/
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
+#import "QSControls.h"
 
-#import "Labels.h"
-#import "Constants.h"
-#import <QuartzCore/QuartzCore.h>
-
-@implementation Labels
+@implementation QSLabels
 
 + (void)trimFrameHeightForLabel:(UILabel *)lblLabel {
 	CGSize objNewSize = [lblLabel.text sizeWithFont:lblLabel.font
